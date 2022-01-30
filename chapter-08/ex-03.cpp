78207c7380c7d4bfffcfddaf57a787178a4834ae
Aykut Schürmeyer <aykutschuermeyer@gmail.com>
2022-01-30 15:26:49 +0100

    feat: chapter 08 ex 03

---
+++ chapter-08/ex-03.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+void findSubsets(std::vector<std::vector<int>> &subsets, std::vector<int> arr, std::vector<int> currentSubset, int index)
+{
+    if (index == arr.size())
+    {
+        subsets.push_back(currentSubset);
+        return;
+    }
+    findSubsets(subsets, arr, currentSubset, index + 1);
+
+    currentSubset.push_back(arr[index]);
+    findSubsets(subsets, arr, currentSubset, index + 1);
+}
+
+int main()
+{
+    std::vector<int> arr = {1, 2, 3};
+    std::vector<std::vector<int>> subsets;
+
+    findSubsets(subsets, arr, {}, 0);
+
+    for (int i = 0; i < subsets.size(); i++)
+    {
+        for (int j = 0; j < subsets[i].size(); j++)
+            std::cout << subsets[i][j] << " ";
+        std::cout << std::endl;
+    }
+}
